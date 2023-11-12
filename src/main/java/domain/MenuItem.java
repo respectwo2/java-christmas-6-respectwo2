5335b875bf044c0fec59bc8eccb06f65f887dd8e
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-12 18:29:55 +0900

    feat: MenuItem 객체 구현 **추후 getter 수정 필요

---
+++ src/main/java/domain/MenuItem.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class MenuItem {
+	
+	private String menu;
+	private int quantity;
+	
+	public MenuItem(String menu, int quantity) {
+		
+		this.menu = menu;
+		this.quantity = quantity;
+		
+	}
+
+	public String getMenu() {
+		return menu;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	
+}
