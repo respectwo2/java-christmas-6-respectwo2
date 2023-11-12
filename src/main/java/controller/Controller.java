756ce1765ae576876800cc54658a0bec92bd27dc
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-12 23:50:16 +0900

    feat: Events 적용하는 checkEvents 메서드 구현중

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,26 +1,36 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import domain.Customer;
+import domain.Events;
 import domain.MenuItem;
 import view.InputView;
 import view.OutputView;
-
+import event.ChristmasDdayDiscountEvent;
+import event.Event;
 public class Controller {
 
 	public static void play() {
-		Customer customer = new Customer();
-		generateCustomer(customer);
-	}
-	
-	private static Customer generateCustomer(Customer inputCustomoer) {
-		InputView input = new InputView();
-		int visitDate = input.readDate();
-		List<MenuItem> menus = input.readMenu();
-		Customer inputCustomer = new Customer(visitDate,menus);
-		OutputView.printMenu(inputCustomer);
-		return inputCustomer;
+		Customer customer = generateCustomer();
+		OutputView.printMenu(customer);
+		OutputView.printBeforeDiscountPrice(customer);
+		checkEvents(customer);
 		
 	}
+
+	private static Customer generateCustomer() {
+        InputView input = new InputView();
+        int visitDate = input.readDate();
+        List<MenuItem> menus = input.readMenu();
+        return new Customer(visitDate, menus, new Events());
+    }
+	
+	private static void checkEvents(Customer customer) {
+		List<MenuItem> menus = customer.getMenuItems();
+		int date = customer.getVisitDate();
+		
+	}
+	
 }
