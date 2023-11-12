747ebb496960348109c0a9bd73511818253b4dca
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-12 18:29:35 +0900

    feat: customer 객체를 생성하는 generateCustomer() 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -5,19 +5,22 @@ import java.util.List;
 import domain.Customer;
 import domain.MenuItem;
 import view.InputView;
+import view.OutputView;
 
 public class Controller {
 
 	public static void play() {
-		generateCustomer();
+		Customer customer = new Customer();
+		generateCustomer(customer);
 	}
 	
-	private static Customer generateCustomer() {
+	private static Customer generateCustomer(Customer inputCustomoer) {
 		InputView input = new InputView();
 		int visitDate = input.readDate();
 		List<MenuItem> menus = input.readMenu();
-		Customer customer = new Customer(visitDate,menus);
-		return customer;
+		Customer inputCustomer = new Customer(visitDate,menus);
+		OutputView.printMenu(inputCustomer);
+		return inputCustomer;
 		
 	}
 }
