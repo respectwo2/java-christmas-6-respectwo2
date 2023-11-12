0b0eb60084e57fe197dcb62962aa6703a3b8e350
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-12 18:28:48 +0900

    feat: readDate & readMenu를 통해 입력 구현 **예외처리 더 필요

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,35 +4,71 @@ import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
+import domain.MenuConst;
 import domain.MenuItem;
 
 public class InputView {
+	
+	private static final String ERROR_ILLEGALARGUMENT_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+	
 	public int readDate() {
-		System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-
-		String input = Console.readLine();
-
-		int inputDate = Integer.parseInt(input);
-
-		return inputDate;
+		while (true) {
+			try {
+				OutputView.printVisitDateInputText();
+				String input = Console.readLine();
+				validateInputNumber(input);
+				int inputDate = Integer.parseInt(input);
+				return inputDate;
+			} catch (IllegalArgumentException e) {
+				OutputView.printErrorMessage(e.getMessage());
+			}
+		}
 	}
+
 
 	public List<MenuItem> readMenu() {
-		System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-
-		List<MenuItem> menuItems = new ArrayList<>();
-
-		String input = Console.readLine();
-
-		String[] menuAndQuantitiy = input.split(",");
-
-		for (String entry : menuAndQuantitiy) {
-			String[] split = entry.split("-");
-			String menu = split[0].trim();
-			int quantity = Integer.parseInt(split[1].trim());
-
-			menuItems.add(new MenuItem(menu, quantity));
+		while (true) {
+			try {
+				OutputView.printMenuItemInputText();
+				List<MenuItem> menuItems = readMenuItems();
+				return menuItems;
+				
+			} catch (IllegalArgumentException e) {
+				OutputView.printErrorMessage(e.getMessage());
+			}
 		}
-		return menuItems;
 	}
+	
+	private List<MenuItem> readMenuItems() {
+	    List<MenuItem> menuItems = new ArrayList<>();
+	    String input = Console.readLine();
+	    String[] menuAndQuantity = input.split(",");
+
+	    for (String entry : menuAndQuantity) {
+	        String[] split = entry.split("-");
+	        String menu = split[0].trim();
+	        validateMenu(menu);
+	        validateInputNumber(split[1].trim());
+	        int quantity = Integer.parseInt(split[1].trim());
+	        menuItems.add(new MenuItem(menu, quantity));
+	    }
+
+	    return menuItems;
+	}
+	
+	private void validateMenu(String input) {
+		for (MenuConst menuItem : MenuConst.values()) {
+			if (menuItem.getName().equalsIgnoreCase(input)) {
+				return;
+			}
+		}
+		throw new IllegalArgumentException(ERROR_ILLEGALARGUMENT_MESSAGE);
+	}
+	
+    private static void validateInputNumber(String input) {
+        if (input.matches("\\d*")) 
+        	return;
+        throw new IllegalArgumentException(ERROR_ILLEGALARGUMENT_MESSAGE);
+    }
+
 }
