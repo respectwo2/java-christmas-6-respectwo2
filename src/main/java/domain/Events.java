0adb7678964a8acec94834fa2bb1cce052fcb474
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-13 16:14:32 +0900

    feat: 출력 포맷 변경 및 Set 사용해 중복 출력 제거

--- src/main/java/domain/Events.java
+++ src/main/java/domain/Events.java
@@ -1,7 +1,10 @@
 package domain;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import event.Event;
 
@@ -20,4 +23,22 @@ public class Events {
     public void addEvent(Event event) {
         eventList.add(event);
     }
+    
+    public String toString(Customer customer) {
+    	
+        StringBuilder sb = new StringBuilder();
+        Set<String> printedEvents = new HashSet<>();
+        for (Event event : eventList) {
+            String eventName = event.getEventName();
+            DecimalFormat df = new DecimalFormat("###,###");
+            String discountAmount = df.format(event.getDiscountAmount(customer));
+            
+            if (!printedEvents.contains(eventName)) {
+                sb.append(eventName).append(": -").append(discountAmount).append("원\n");
+                printedEvents.add(eventName);
+            }
+        }
+        return sb.toString();
+    }
+    
 }
