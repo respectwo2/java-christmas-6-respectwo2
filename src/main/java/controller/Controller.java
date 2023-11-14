a8df7c61dbb3291073a461c8e0ff95bd43a9ec08
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-14 14:41:50 +0900

    feat: Customer 객체 생성과 이벤트 적용 통합

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,33 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.Customer;
-import domain.Events;
+import domain.DiscountEvent;
+import domain.DiscountWeekEvent;
 import domain.MenuItem;
 import view.InputView;
 import view.OutputView;
 import event.ChristmasDdayDiscountEvent;
-import event.Event;
+import event.GiftEvent;
+import event.WeekdayDiscountEvent;
+import event.WeekendDiscountEvent;
 public class Controller {
 
 	public static void play() {
 		Customer customer = generateCustomer();
 		OutputView.printMenu(customer);
 		OutputView.printBeforeDiscountPrice(customer);
-		checkEvents(customer);
-		
+//	    OutputView.printEvents(customer);
+        OutputView.printEvents(customer.getEvents());
+        OutputView.printTotalEventPrice(customer);
+
 	}
 
+
 	private static Customer generateCustomer() {
+		
         InputView input = new InputView();
         int visitDate = input.readDate();
         List<MenuItem> menus = input.readMenu();
-        return new Customer(visitDate, menus, new Events());
+        Customer customer = new Customer(visitDate, menus, new ArrayList<>());
+        eventApply(customer);
+        
+        return customer;
     }
 	
-	private static void checkEvents(Customer customer) {
-		List<MenuItem> menus = customer.getMenuItems();
-		int date = customer.getVisitDate();
-		
-	}
-	
+    public static void eventApply(Customer customer) {
+        List<DiscountEvent> events = customer.getEvents();
+
+        DiscountEvent christmasEvent = new ChristmasDdayDiscountEvent();
+        DiscountEvent giftEvent = new GiftEvent();
+        DiscountEvent weekdayEvent = new WeekdayDiscountEvent();
+        DiscountEvent weekendEvent = new WeekendDiscountEvent();
+
+        applyEventIfApplicable(christmasEvent, customer, events);
+        applyEventIfApplicable(giftEvent, customer, events);
+        applyEventIfApplicable(weekdayEvent, customer, events);
+        applyEventIfApplicable(weekendEvent, customer, events);
+    }
+
+    private static void applyEventIfApplicable(DiscountEvent event, Customer customer, List<DiscountEvent> events) {
+        if (event.isApplicable(customer)) {
+        	event.calculateAndSetDiscountAmount(customer);
+            events.add(event);
+        }
+    }
+
 }
