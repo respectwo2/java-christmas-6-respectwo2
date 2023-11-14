b0729fd6b3efa54972098f8a55687f84e889b9f3
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-15 01:44:24 +0900

    style: 날짜 및 할인 금액 상수화

--- src/main/java/event/ChristmasDdayDiscountEvent.java
+++ src/main/java/event/ChristmasDdayDiscountEvent.java
@@ -6,7 +6,10 @@ import domain.DiscountEvent;
 public class ChristmasDdayDiscountEvent extends DiscountEvent {
 
     private static final String EVENT_NAME = "크리스마스 디데이 할인";
-
+    private static final int EVENT_START = 1;
+    private static final int EVENT_END = 25;
+    private static final int CHRISTMAS_DDAY_DISCOUNT_STARTPRICE = 1000;
+    
     public ChristmasDdayDiscountEvent() {
         super(EVENT_NAME, 0);
     }
@@ -20,8 +23,8 @@ public class ChristmasDdayDiscountEvent extends DiscountEvent {
     @Override
     public int calculateDiscountAmount(Customer customer) {
         int visitDate = customer.getVisitDate();
-        if (visitDate >= 1 && visitDate <= 25) {
-            return 1000 + (visitDate - 1) * 100;
+        if (visitDate >= EVENT_START && visitDate <= EVENT_END) {
+            return CHRISTMAS_DDAY_DISCOUNT_STARTPRICE + (visitDate - 1) * 100;
         }
         return 0;
     }
